808eb1b264fa0676bbdc44733c205fef1ad9fca1
Muhammad Mushlih Al 'Aadil <mmushlih.alaadil@gmail.com>
2024-06-12 21:24:26 +0700

    feat:redis config

---
+++ server/config/redis.js
@@ -0,0 +1,10 @@
+const { Redis } = require("ioredis");
+
+const redis = new Redis({
+  port: 10508, // Redis port
+  host: process.env.REDIS_HOST,
+  username: "default",
+  password: process.env.REDIS_PASSWORD,
+});
+
+module.exports = redis;
