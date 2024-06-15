5c6b2a929d343ecf3ed40abcfdc914fe920cc953
Yugo <ilhamyugooo@gmail.com>
2024-06-15 14:45:08 +0700

    fix: create store

--- server/models/StoreModels.js
+++ server/models/StoreModels.js
@@ -3,78 +3,105 @@ const { database } = require("../config/mongodb");
 const redis = require("../config/redis");
 
 class Store {
-    static collection() {
-        return database.collection("stores");
-    }
+  static collection() {
+    return database.collection("stores");
+  }
 
-    static async getAllStores(_id) {
-        const stores = await Store.collection()
-        .aggregate([
-            {
-                '$match': {
-                  'userId': new ObjectId(_id)
-                }
-              }
-        ]).toArray();
+  static async getAllStores(_id) {
+    const stores = await Store.collection()
+      .aggregate([
+        {
+          $match: {
+            userId: new ObjectId(_id),
+          },
+        },
+      ])
+      .toArray();
 
-        return stores;
-    }
+    return stores;
+  }
 
-    static async getStoreById(_id) {
-        const store = await Store.collection().findOne({ _id: new ObjectId(_id) });
+  static async getStoreById(_id) {
+    const store = await Store.collection().findOne({ _id: new ObjectId(_id) });
 
-        return store;
-    }
+    return store;
+  }
 
-    static async createStore(newStore) {
-        newStore.since = new Date(newStore.since);
-        newStore.userId = new ObjectId(newStore.userId)
+  static async createStore(newStore) {
+    newStore.since = new Date(newStore.since);
+    newStore.userId = new ObjectId(newStore.userId);
 
-        if (newStore.name.length === 0) throw new Error("Store name cannot be empty");
-        if (newStore.description.length === 0) throw new Error("Store description cannot be empty");
-        if (newStore.phoneNumber.length === 0) throw new Error("Store phone number cannot be empty");
-        if (newStore.address.length === 0) throw new Error("Store address cannot be empty");
+    if (newStore.name.length === 0)
+      throw new Error("Store name cannot be empty");
+    if (newStore.description.length === 0)
+      throw new Error("Store description cannot be empty");
+    if (newStore.phoneNumber.length === 0)
+      throw new Error("Store phone number cannot be empty");
+    if (newStore.address.length === 0)
+      throw new Error("Store address cannot be empty");
 
-        const store = await Store.collection().insertOne(newStore);
+    const store = await Store.collection().insertOne(newStore);
 
-        await redis.del("stores");
+    const filter = { _id: new ObjectId(newStore.userId) };
+    const updateDoc = {
+      $set: {
+        isNewAccount: false,
+      },
+    };
 
-        return store
-    }
+    const updateUser = await database
+      .collection("users")
+      .updateOne(filter, updateDoc);
 
-    static async updateStore(_id, name, description, phoneNumber, address, since) {
-        since = new Date(since);
+    await redis.del("stores");
 
-        if (name.length === 0) throw new Error("Store name cannot be empty");
-        if (description.length === 0) throw new Error("Store description cannot be empty");
-        if (phoneNumber.length === 0) throw new Error("Store phone number cannot be empty");
-        if (address.length === 0) throw new Error("Store address cannot be empty");
+    return store;
+  }
 
-        const store = await Store.collection().findOneAndUpdate(
-            { _id: new ObjectId(_id) },
-            {
-                $set: {
-                    name,
-                    description,
-                    phoneNumber,
-                    address,
-                    since
-                }
-            },
-            { returnOriginal: false }
-        );
+  static async updateStore(
+    _id,
+    name,
+    description,
+    phoneNumber,
+    address,
+    since
+  ) {
+    since = new Date(since);
 
-        if (!store) throw new Error("Store not found");
+    if (name.length === 0) throw new Error("Store name cannot be empty");
+    if (description.length === 0)
+      throw new Error("Store description cannot be empty");
+    if (phoneNumber.length === 0)
+      throw new Error("Store phone number cannot be empty");
+    if (address.length === 0) throw new Error("Store address cannot be empty");
 
-        const updatedStore = Store.collection().findOne({ _id: new ObjectId(_id) });
+    const store = await Store.collection().findOneAndUpdate(
+      { _id: new ObjectId(_id) },
+      {
+        $set: {
+          name,
+          description,
+          phoneNumber,
+          address,
+          since,
+        },
+      },
+      { returnOriginal: false }
+    );
 
-        return updatedStore;
-    }
-    
-    static async deleteStore(_id) {
-        const store = await Store.collection().findOneAndDelete({ _id: new ObjectId(_id) });
-        if (!store) throw new Error("Store not found");
-    }
+    if (!store) throw new Error("Store not found");
+
+    const updatedStore = Store.collection().findOne({ _id: new ObjectId(_id) });
+
+    return updatedStore;
+  }
+
+  static async deleteStore(_id) {
+    const store = await Store.collection().findOneAndDelete({
+      _id: new ObjectId(_id),
+    });
+    if (!store) throw new Error("Store not found");
+  }
 }
 
 module.exports = Store;
